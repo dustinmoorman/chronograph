b7d28906660b8e3ce2f95057f299b20b9b5d8e20
Dustin Moorman <dustin.moorman@gmail.com>
2013-03-01 21:24:24 -0600

    Added timeJSON.php to generate JSON data of the current server time.

---
+++ timeJSON.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Dustin Moorman <dustin.moorman@gmail.com>
+ * Server time JSON for the clock engine.
+ */
+
+header('Expires: Thu, 18 Sep 2008 00:00:00 GMT');
+header('Cache-Control: no-cache, must-revalidate');
+header('Content-Type: application/json');
+
+echo json_encode(
+    array(
+        'hour'   => date('H'),
+        'minute' => date('i'),
+        'second' => date('s')
+    )
+);
