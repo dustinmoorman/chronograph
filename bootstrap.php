f4d34662a618b872677421913cf5921d9d687677
Dustin Moorman <dustin.moorman@gmail.com>
2016-04-17 09:37:58 -0500

    to interpolate or not interpolate thats the question

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@
 */
 
 spl_autoload_register(function($class_name) {
-	if (file_exists("classes/$class_name.php")) {
-		include_once "classes/$class_name.php";
+	if (file_exists("classes/{$class_name}.php")) {
+		include_once "classes/{$class_name}.php";
 	}
 });
