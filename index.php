3cd5e478e00362f4f6cafd6e84ab999c9750e065
Dustin Moorman <dustin.moorman@gmail.com>
2016-04-18 11:44:33 -0500

    seconds to small array style

--- index.php
+++ index.php
@@ -24,33 +24,31 @@
 
             require_once 'classes/SpanGenerator.php';
 
-            $Seconds = new SpanGenerator(
-                array(
-                    '1' => 'one',
-                    '2' => 'two',
-                    '3' => 'three',
-                    '4' => 'four',
-                    '5' => 'five',
-                    '6' => 'six',
-                    '7' => 'seven',
-                    '8' => 'eight',
-                    '9' => 'nine',
-                    '10' => 'ten',
-                    '11' => 'eleven',
-                    '12' => 'twelve',
-                    '13' => 'thirteen',
-                    '14' => 'fourteen',
-                    '15' => 'fifteen',
-                    '16' => 'sixteen',
-                    '17' => 'seventeen',
-                    '18' => 'eighteen',
-                    '19' => 'nineteen',
-                    '20' => 'twenty',
-                    '30' => 'thirty',
-                    '40' => 'fourty',
-                    '50' => 'fifty',
-                )
-            );
+            $Seconds = new SpanGenerator([
+	            '1' => 'one',
+	            '2' => 'two',
+	            '3' => 'three',
+	            '4' => 'four',
+	            '5' => 'five',
+	            '6' => 'six',
+	            '7' => 'seven',
+	            '8' => 'eight',
+	            '9' => 'nine',
+	            '10' => 'ten',
+	            '11' => 'eleven',
+	            '12' => 'twelve',
+	            '13' => 'thirteen',
+	            '14' => 'fourteen',
+	            '15' => 'fifteen',
+	            '16' => 'sixteen',
+	            '17' => 'seventeen',
+	            '18' => 'eighteen',
+	            '19' => 'nineteen',
+	            '20' => 'twenty',
+	            '30' => 'thirty',
+	            '40' => 'fourty',
+	            '50' => 'fifty'
+            ]);
 
             $Minutes = clone $Seconds;
 
